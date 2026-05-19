349648f497ad3c4a8d6ef85fcffb947918515167
thewelox <technicalgamer442@gmail.com>
2026-05-19 23:42:51 +0530

    fixed railway port

--- index.js
+++ index.js
@@ -87,6 +87,8 @@ function leaveRoom(socket) {
   }
 }
 
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
