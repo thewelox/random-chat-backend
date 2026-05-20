685437e059ca5ceff597b203d8a1e01540251955
thewelox <technicalgamer442@gmail.com>
2026-05-20 23:59:55 +0530

    major backend stability upgrade

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+
 const http = require("http");
+
 const { Server } = require("socket.io");
+
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("VEIL backend running");
+});
 
 const server = http.createServer(app);
 
@@ -24,28 +31,61 @@ io.on("connection", (socket) => {
   socket.on("join", (data) => {
     socket.nickname = data.nickname;
 
-    waitingUsers.push(socket);
+    socket.gender = data.gender;
+
+    const alreadyWaiting =
+      waitingUsers.find(
+        (user) => user.id === socket.id
+      );
+
+    if (!alreadyWaiting) {
+      waitingUsers.push(socket);
+    }
 
     matchUsers();
   });
 
- socket.on("message", (data) => {
-  io.to(socket.roomId).emit("message", {
-    text: data.text,
-    nickname: socket.nickname,
-    sender: socket.id,
-    replyTo: data.replyTo || null,
+  socket.on("message", (data) => {
+    if (!socket.roomId) return;
+
+    io.to(socket.roomId).emit(
+      "message",
+      {
+        id: Date.now(),
+
+        text: data.text,
+
+        nickname: socket.nickname,
+
+        gender: socket.gender,
+
+        sender: socket.id,
+
+        replyTo:
+          data.replyTo || null,
+      }
+    );
   });
-});
 
   socket.on("typing", () => {
-    socket.to(socket.roomId).emit("typing");
+    if (!socket.roomId) return;
+
+    socket
+      .to(socket.roomId)
+      .emit("typing");
   });
 
   socket.on("next", () => {
     leaveRoom(socket);
 
-    waitingUsers.push(socket);
+    const alreadyWaiting =
+      waitingUsers.find(
+        (user) => user.id === socket.id
+      );
+
+    if (!alreadyWaiting) {
+      waitingUsers.push(socket);
+    }
 
     matchUsers();
   });
@@ -53,35 +93,54 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     leaveRoom(socket);
 
-    waitingUsers = waitingUsers.filter(
-      (user) => user.id !== socket.id
+    waitingUsers =
+      waitingUsers.filter(
+        (user) =>
+          user.id !== socket.id
+      );
+
+    console.log(
+      "Disconnected:",
+      socket.id
     );
   });
 });
 
 function matchUsers() {
   while (waitingUsers.length >= 2) {
-    const user1 = waitingUsers.shift();
+    const user1 =
+      waitingUsers.shift();
 
-    const user2 = waitingUsers.shift();
+    const user2 =
+      waitingUsers.shift();
+
+    if (!user1 || !user2) return;
 
     const roomId = `room-${user1.id}-${user2.id}`;
 
     user1.join(roomId);
+
     user2.join(roomId);
 
     user1.roomId = roomId;
+
     user2.roomId = roomId;
 
-    io.to(roomId).emit("matched");
+    io.to(roomId).emit(
+      "matched"
+    );
+
+    console.log(
+      `Matched ${user1.nickname} with ${user2.nickname}`
+    );
   }
 }
 
 function leaveRoom(socket) {
   if (socket.roomId) {
-    socket.to(socket.roomId).emit(
-      "stranger_left"
-    );
+    socket
+      .to(socket.roomId)
+      .emit("stranger_left");
 
     socket.leave(socket.roomId);
 
@@ -89,8 +148,11 @@ function leaveRoom(socket) {
   }
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT =
+  process.env.PORT || 3001;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
